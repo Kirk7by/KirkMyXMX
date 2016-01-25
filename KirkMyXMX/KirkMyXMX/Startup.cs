d75596dcd4e9066e01be2e3c74d08822a851c1c0
Kirill <kiiirrrk@gmail.com>
2016-01-26 02:45:05 +0300

    CREATE Site (+AZURE)

---
+++ KirkMyXMX/KirkMyXMX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KirkMyXMX.Startup))]
+namespace KirkMyXMX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
